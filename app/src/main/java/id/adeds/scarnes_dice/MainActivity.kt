ef0d6b1fe950ed82413a02fa6ec7c51cdc88731e
adeds <adeyds@gmail.com>
2020-11-30 13:40:31 +0700

    initial

---
+++ app/src/main/java/id/adeds/scarnes_dice/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.adeds.scarnes_dice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
