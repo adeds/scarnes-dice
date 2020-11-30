01eaf0d9514cc2244c1fca9fd8b6fcd476c1d4fb
adeds <adeyds@gmail.com>
2020-11-30 14:12:08 +0700

    milestone 2 : roll dice and reflect to view

--- app/src/main/java/id/adeds/scarnes_dice/MainActivity.kt
+++ app/src/main/java/id/adeds/scarnes_dice/MainActivity.kt
@@ -1,11 +1,43 @@
 package id.adeds.scarnes_dice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import id.adeds.scarnes_dice.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val userScore = 0
+    private val computerScore = 0
+    private val userTurnScore = 0
+    private val computerTurnScore = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initClickListener()
+    }
+
+    private fun initClickListener() = binding.apply {
+        btnRoll.setOnClickListener {
+            updateDice(Random.nextInt(5))
+        }
+    }
+
+    private fun updateDice(dice: Int) {
+        val imgDice = when (dice) {
+            0 -> R.drawable.dice1
+            1 -> R.drawable.dice2
+            2 -> R.drawable.dice3
+            3 -> R.drawable.dice4
+            4 -> R.drawable.dice5
+            5 -> R.drawable.dice6
+            else -> null
+        }
+        binding.imgDice.setImageDrawable(imgDice?.let { ContextCompat.getDrawable(this, it) })
     }
 }
